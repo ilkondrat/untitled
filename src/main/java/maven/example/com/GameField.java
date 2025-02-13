8899d2e6b744b1343369c4d29e79dac07f4da318
illiakondrat <i.kondrat988@gmail.com>
2025-02-13 13:17:44 +0100

    first commit

--- src/main/java/maven/example/com/GameField.java
+++ src/main/java/maven/example/com/GameField.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.Random;
+// тестовый коммит для проверки
 
 public class GameField extends JPanel implements ActionListener {
     private final int SIZE = 512;

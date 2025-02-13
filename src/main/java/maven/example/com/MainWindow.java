9e598b7f8141eefc50e66f80494c4b330ea5dfd5
illiakondrat <i.kondrat988@gmail.com>
2025-02-13 13:23:23 +0100

    second commit

--- src/main/java/maven/example/com/MainWindow.java
+++ src/main/java/maven/example/com/MainWindow.java
@@ -22,6 +22,6 @@ public class MainWindow extends JFrame {
 
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
-
+        // все должно работать
     }
 }
